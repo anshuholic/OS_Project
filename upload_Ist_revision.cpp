23d8f8d7753f0fd781ce497a2c1e49dd4e7d4821
ANSHUMAN BHUSHAN SINGH <49102200+anshuholic@users.noreply.github.com>
2019-04-18 21:18:16 +0530

    Add files via upload
    Taken variable for entering arrival time burst time process no and priority.
    Only done the basic part.

---
+++ upload_Ist_revision.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Process_no[10],Burst_time[10],Turn_around_time[10],Waiting_time[10];
+    int Arrival_time[10],Priority[10],i,j,Number_of_Process,tot=0,flag,temp,Average_waiting_time,Average_turn_around_time;
+    printf("Enter Number of Process:");
+    scanf("%d",&Number_of_Process);
+}
